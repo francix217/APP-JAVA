642e6bc5de9ac3cb1950c1dba224b43bb9e558d3
Franco <mirandafranco217@gmail.com>
2026-04-17 23:34:57 -0300

    Armado de la pagina y mejoras

---
+++ src/main/java/ar/com/franco/AppJava/controllers/Productos.java
@@ -0,0 +1,13 @@
+package ar.com.franco.AppJava.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Productos {
+	
+    @GetMapping("/productos")
+    public String productos() {
+        return "productos";
+    }
+}
