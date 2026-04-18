88d21688e0badcacfaca5b632e1fe37ea56f7e80
Franco <mirandafranco217@gmail.com>
2026-04-18 14:05:41 -0300

    Arreglos

--- src/main/java/ar/com/franco/AppJava/controllers/Productos.java
+++ src/main/java/ar/com/franco/AppJava/controllers/Productos.java
@@ -1,13 +1,28 @@
 package ar.com.franco.AppJava.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import ar.com.franco.AppJava.form.ProductoForm;
+
 
 @Controller
 public class Productos {
 	
-    @GetMapping("/productos")
-    public String productos() {
-        return "productos/productos";
+	@GetMapping("/productos")
+	public String productos(Model model) {
+	    model.addAttribute("form", new ProductoForm());
+	    return "productos/productos";
+	}
+    
+    @PostMapping
+    public String guardar(ProductoForm form) {
+        System.out.println(form.getNombre());
+        System.out.println(form.getPrecio());
+        return "redirect:/productos";
     }
+    
+    
 }
---
+++ src/main/java/ar/com/franco/AppJava/form/ProductoForm.java
@@ -0,0 +1,20 @@
+package ar.com.franco.AppJava.form;
+
+public class ProductoForm {
+	
+	private String nombre;
+	private double precio;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+}
