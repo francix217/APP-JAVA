11bc7ad55a1a32a96165c315748fcfd432631e94
Franco <mirandafranco217@gmail.com>
2026-04-24 19:25:19 -0300

    DISEÑOS, CAMBIOS Y PANEL ADMIN

--- src/main/java/ar/com/franco/AppJava/controllers/Registro.java
+++ src/main/java/ar/com/franco/AppJava/controllers/Registro.java
@@ -1,21 +1,42 @@
 package ar.com.franco.AppJava.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import ar.com.franco.AppJava.domain.Usuario;
+import ar.com.franco.AppJava.emuns.Genero;
+import ar.com.franco.AppJava.form.UserForm;
+import ar.com.franco.AppJava.servicios.UserService;
+
 @Controller
-public class Registro{
+public class Registro {
+
+    @Autowired
+    private UserService servicio;
 
     @GetMapping("/registro")
-    public String mostrarRegistro() {
-        return "registro/registro";
+    public String mostrarRegistro(Model model) {
+        model.addAttribute("form", new UserForm());
+        return "registro";
     }
 
     @PostMapping("/registro")
-    public String procesarRegistro() {
+    public String registrar(@ModelAttribute("form") UserForm form) {
 
-        System.out.println("Usuario registrado");
+        form.setRol("USER");
+
+        Usuario user = new Usuario(
+            form.getUsuario(),
+            form.getContrasena(),
+            Genero.valueOf(form.getGenero()),
+            form.getRol()
+        );
+
+        servicio.guardarUsuario(user);
 
         return "redirect:/login";
     }
