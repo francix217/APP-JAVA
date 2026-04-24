be98771dbed1280ed8496007bf0ca2ffdfe9798b
Franco <mirandafranco217@gmail.com>
2026-04-24 16:31:47 -0300

    DISEÑOS, CAMBIOS Y PANEL ADMIN

--- src/main/java/ar/com/franco/AppJava/controllers/admin/UserAdminController.java
+++ src/main/java/ar/com/franco/AppJava/controllers/admin/UserAdminController.java
@@ -1,11 +1,15 @@
 package ar.com.franco.AppJava.controllers.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import ar.com.franco.AppJava.form.UserForm;
 
 @Controller
 public class UserAdminController {
 
+    private static final String FORM_ATTRIBUTE = "form";
     private static final String PATH_PAGES_URL = "admin/users";
     private static final String PATH_CONTEXT_URL = "/adm/users";
 
@@ -15,7 +19,8 @@ public class UserAdminController {
     }
 
     @GetMapping(PATH_CONTEXT_URL + "/edit")
-    public String edit() {
+    public String edit(Model model) {
+        model.addAttribute(FORM_ATTRIBUTE, new UserForm());
         return PATH_PAGES_URL + "/form";
     }
 }
---
+++ src/main/java/ar/com/franco/AppJava/form/UserForm.java
@@ -0,0 +1,40 @@
+package ar.com.franco.AppJava.form;
+
+public class UserForm {
+    private String usuario;
+    private String contrasena;
+    private String rol;
+    private String genero;
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
