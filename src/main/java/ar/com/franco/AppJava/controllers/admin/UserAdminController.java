70ea45442bf86a9d023b2e0c5aa2174b8bfe3ad5
Franco <mirandafranco217@gmail.com>
2026-04-24 16:53:38 -0300

    DISEÑOS, CAMBIOS Y PANEL ADMIN

--- src/main/java/ar/com/franco/AppJava/controllers/admin/UserAdminController.java
+++ src/main/java/ar/com/franco/AppJava/controllers/admin/UserAdminController.java
@@ -3,6 +3,8 @@ package ar.com.franco.AppJava.controllers.admin;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import ar.com.franco.AppJava.form.UserForm;
 
@@ -22,5 +24,16 @@ public class UserAdminController {
     public String edit(Model model) {
         model.addAttribute(FORM_ATTRIBUTE, new UserForm());
         return PATH_PAGES_URL + "/form";
+    }
+    
+    @PostMapping(PATH_CONTEXT_URL + "/save")
+    public String save(@ModelAttribute("form") UserForm formulario) {
+
+        System.out.println(formulario.getUsuario());
+        System.out.println(formulario.getContrasena());
+        System.out.println(formulario.getRol());
+        System.out.println(formulario.getGenero());
+
+        return "redirect:/adm/users";
     }
 }
