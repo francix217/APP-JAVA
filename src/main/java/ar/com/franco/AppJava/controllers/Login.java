7adab443387768227f3692429d2ab15f532d41d1
Franco <mirandafranco217@gmail.com>
2026-05-18 11:14:40 -0300

    Cambios

--- src/main/java/ar/com/franco/AppJava/controllers/Login.java
+++ src/main/java/ar/com/franco/AppJava/controllers/Login.java
@@ -1,16 +1,35 @@
 package ar.com.franco.AppJava.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import ar.com.franco.AppJava.form.UserForm;
+import ar.com.franco.AppJava.form.validators.LoginFormValidator;
+import ar.com.franco.AppJava.servicios.UserService;
 
 @Controller
 public class Login {
 
+    public static final String LOGIN_URL = "/login";
+    private static final String FORM_ATTRIBUTE = "form";
+    
+    @Autowired
+    private UserService service;
+
+    @Autowired
+    private LoginFormValidator validator;
+    
+    @InitBinder(value = FORM_ATTRIBUTE)
+    void initFormValidator(WebDataBinder binder) {
+        binder.addValidators(this.validator);
+    }
+		
     @GetMapping("/login")
     public String login(Model model) {
         model.addAttribute("form", new UserForm());
